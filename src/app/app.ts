0750e2b0aadb75707df52f0eaa24980d40173061
Sonia Mohamed <sonia.mohamed41@gmail.com>
2026-07-23 08:20:46 +0300

    Updated about, contact and app components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Home } from './Components/home/home';
 import { Navbar } from './Components/navbar/navbar';
 import { Footer } from './Components/footer/footer';
+import { About } from './Components/about/about';
+import { Contact } from './Components/contact/contact';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Home,Navbar,Footer],
+  imports: [Home,About,Contact,Navbar,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
